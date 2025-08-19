d4bf5161a46d25cb56cf41456f4aae6a6e94d410
vis4rd <alagaesia.vrael@gmail.com>
2025-08-19 21:00:12 +0200

    Implementation of chat window (part 2)

--- app/src/components/ChatWindow.vue
+++ app/src/components/ChatWindow.vue
@@ -1,15 +1,76 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import BlockWrapper from "@/components/BlockWrapper.vue";
+    import ChatMessage from "@/components/ChatMessage.vue";
+    import ElementWrapper from "@/components/ElementWrapper.vue";
+    import { Button } from "@/components/ui/button";
+    import { Input } from "@/components/ui/input";
+    import { useConversationListStore } from "@/stores/conversation_list";
+    import { Icon } from "@iconify/vue";
+    import { computed, ref } from "vue";
+
+    const conversationListStore = useConversationListStore();
+
+    const input = ref("");
+    const inputLength = computed(() => input.value.trim().length);
+
+    function isMessageValid() {
+        return inputLength.value > 0 && inputLength.value <= 100;
+    }
+    function sendMessage() {
+        if (isMessageValid()) {
+            // TODO
+            input.value = "";
+        }
+    }
+</script>
 
 <template>
-    <div class="chat-window">
-        <slot />
+    <div class="column">
+        <BlockWrapper class="chat-window">
+            <!-- TODO: conversation name -->
+            <!-- TODO: number of chatters -->
+            Chat
+
+            <span>Selected: '{{ conversationListStore.selectedChat?.id }}'</span>
+            <span>Index: '{{ conversationListStore.selectedChat?.index }}'</span>
+            <span>PDA: '{{ conversationListStore.selectedChat?.pda }}'</span>
+
+            <ChatMessage user>hello</ChatMessage>
+            <ChatMessage>hi there!</ChatMessage>
+        </BlockWrapper>
+
+        <div class="message-input">
+            <form class="flex w-full items-center space-x-2" @submit.prevent="sendMessage()">
+                <Input v-model="input" placeholder="Type a message..." />
+                <ElementWrapper class="character-count">{{ 100 - inputLength }}</ElementWrapper>
+                <Button variant="default" size="icon" aria-label="Send the message" :disabled="!isMessageValid()">
+                    <Icon icon="fluent:send-24-regular" class="size-5"></Icon>
+                </Button>
+            </form>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    .chat-window {
+    .column {
         display: flex;
         flex-direction: column;
         width: 100%;
+        gap: 1rem;
+    }
+    .chat-window {
+        display: flex;
+        flex-direction: column;
+        margin-block: 0;
+        width: 100%;
+    }
+    .message-input {
+        display: flex;
+        flex-direction: row;
+        gap: 0.5rem;
+        align-items: center;
+    }
+    .character-count {
+        color: v-bind('(100 - inputLength) < 20 ? "var(--color-destructive)" : "var(--color-foreground)"');
     }
 </style>
--- app/src/views/ChatView.vue
+++ app/src/views/ChatView.vue
@@ -1,30 +1,12 @@
 <script setup lang="ts">
-    import BlockWrapper from "@/components/BlockWrapper.vue";
-    import ChatMessage from "@/components/ChatMessage.vue";
     import ChatWindow from "@/components/ChatWindow.vue";
     import ConversationList from "@/components/ConversationList.vue";
-    import { useConversationListStore } from "@/stores/conversation_list";
-
-    const conversationListStore = useConversationListStore();
 </script>
 
 <template>
     <div class="chat-view">
         <ConversationList class="list" />
-        <BlockWrapper class="chat-window-block">
-            <!-- TODO: conversation name -->
-            <!-- TODO: number of chatters -->
-            Chat
-
-            <span>Selected: '{{ conversationListStore.selectedChat?.id }}'</span>
-            <span>Index: '{{ conversationListStore.selectedChat?.index }}'</span>
-            <span>PDA: '{{ conversationListStore.selectedChat?.pda }}'</span>
-
-            <ChatWindow>
-                <ChatMessage user>hello</ChatMessage>
-                <ChatMessage>hi there!</ChatMessage>
-            </ChatWindow>
-        </BlockWrapper>
+        <ChatWindow />
     </div>
 </template>
 
@@ -40,11 +22,5 @@
     .list {
         min-width: 200px;
         flex: 0 0 30%;
-    }
-
-    .chat-window-block {
-        display: flex;
-        margin-block: 0;
-        flex: 1;
     }
 </style>
