ac1803a150bd479e7db8f9d24524dbf6328a2be0
vis4rd <alagaesia.vrael@gmail.com>
2025-08-19 21:00:48 +0200

    Align ElementWrapper with shadcn-vue style

--- app/src/components/ElementWrapper.vue
+++ app/src/components/ElementWrapper.vue
@@ -11,12 +11,18 @@
         display: flex;
         flex-direction: column;
         place-items: center;
+        place-content: center;
         box-sizing: border-box;
 
-        padding: 0.5rem;
+        font-size: var(--text-sm);
+        line-height: var(--tw-leading, var(--text-sm--line-height));
+        padding-block: calc(var(--spacing) * 1);
+        padding-inline: calc(var(--spacing) * 3);
+        height: calc(var(--spacing) * 9);
 
-        border-style: solid;
-        border-radius: 6px;
+        border-color: var(--input);
+        border-style: var(--tw-border-style);
+        border-radius: calc(var(--radius) - 2px);
         border-width: 1px;
     }
 </style>
